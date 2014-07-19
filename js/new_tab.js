8d72046a9de2203a960ed5ae3dc04df5acd53f18
DWu39 <dwu399@gmail.com>
2014-07-19 12:16:33 -0400

    friends on new tab started

--- js/new_tab.js
+++ js/new_tab.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
+  var source   = $("#friend_block_template").html();
+  var template = Handlebars.compile(source);
 
+  var context = {title: "My New Post", body: "This is my first post!"}
+  var html    = template(context);
 });
