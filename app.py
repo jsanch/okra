fbb226bf43f59e6eb0398656d24e665344b6440e
root <root@app.grasscat.org>
2014-07-18 21:17:51 -0400

    upload file working

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, redirect, url_for, jsonify
+from flask import Flask, request, redirect, render_template, url_for, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
 
 
@@ -7,10 +7,8 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'HELLO'
 
+############################## get tab ################################
 tab = {'invitation_id' : 1,
         'user_id' : 2,
         'tab_id' : 5,
@@ -21,40 +19,67 @@ tab = {'invitation_id' : 1,
 def get_polltab():
     return jsonify( { 'tab': tab } )
 
-############################# UPLAOD IMAGE #############################
-UPLOAD_FOLDER = 'images'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+########################################################################
 
+############################## get tab ################################
+
+
+
+
+
+########################################################################
+
+
+
+############################# UPLAOD IMAGE #############################
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'images/'
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+# For a given file, return whether it's an allowed type or not
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+
+# This route will show a form to perform an AJAX request
+# jQuery is loaded to execute the request and update the
+# value of the operation
+@app.route('/img-upload')
+def index():
+    return render_template('img-upload.html')
+
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
+
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 ########################################################################
 
 ################################ DB ####################################
 
-
 ########################################################################
 
-
-
 if __name__ == '__main__':
-
     app.run(debug=True, host='0.0.0.0')
