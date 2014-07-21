3a79d8d191a2ede00b08d65f92437dd2eb81b8a3
mzw4 <mzw4@cornell.edu>
2014-07-21 00:48:50 -0400

    tab page is formatted nicely, hooking up

--- static/js/new_tab.js
+++ static/js/new_tab.js
@@ -58,87 +58,20 @@ $(document).ready(function() {
     }
   };
 
-  // send file url to backend to parse
-  function createTab() {
-    var newTab = {
-      master_id : user_id,
-      group : {},
-      file_url : ''
-    };
-    $.post('http://app.grasscat.org:5000/ajax/create_tab', {tab: newTab})
-    .done(function(data) {
-    });
-  }
-
+  // Creating a new tab, opens Tab View and passes it the new tab
   $('#uploadform').submit(function(e){
     e.preventDefault();
 
     // $(this).ajaxSubmit({
     //   success: function(responseText, statusText, xhr, $form) {
-        closeNewTabView();
+        var tab_id = '53cc7b70d2a57d14a1f58c5d'; // CHANGE
+        closeNewTabView(tab_id);
     //   }
     // });
   });
 
   $('#js-create-tab').click(function(){
     $('#uploadform').submit();
-    // createTab();
   });
-
-  // // get friends and populate the add friends modal
-  // function openAddFriends(user_id) {
-  //   var friends_list = getFriends(user_id);
-  //   var AddFriendsTemplate = Handlebars.compile($('#add_friends_template').html());
-  //   $('#add_friends_body').html(AddFriendsTemplate({friends : friends_list}));
-  //   // toggle friends already added to list
-  //   $('.js-add-friend').each(function() {
-  //     var friend = $('.js-friend', this);
-  //     if (friend.data('id') in friendsToAdd) {
-  //       $('.glyphicon-ok-circle', this).toggle();
-  //     }
-  //   });
-  //   // bind add friend action to each friend
-  //   $('.js-add-friend').on('click', function() {
-  //     addFriend($(this));
-  //   });
-  //   // show modal
-  //   $('#js-add-friends-modal').modal();
-  // }
-
-  // // returns dictionary of friends with their ids and names
-  // function getFriends(user_id) {
-  //   return {
-  //       // user_id : user_name
-  //       1 : {name : 'Mikey Big Wang'},
-  //       2 : {name : 'Vinay Fuck This Farias'},
-  //       3 : {name : 'Steven Nuggers Han'},
-  //       4 : {name : 'Jaime Hipster Sanchez'},
-  //       5 : {name : 'Dan The Boss Wu'}
-  //     };
-  //   $.get('http://app.grasscat.org:5000/ajax/get_friends', {user_id : user_id})
-  //   .done(function(data) {
-  //     return data;
-  //   });
-  // }
-
-  // // add the friend row to the global set or delete it if it's already in there
-  // function addFriend(friend) {
-  //   $('.glyphicon-ok-circle', friend).toggle();
-  //   var selectedID = $('.js-friend', friend).data('id');
-  //   if (selectedID in friendsToAdd) {
-  //     delete friendsToAdd[selectedID];
-  //   } else {
-  //     friendsToAdd[selectedID] = {name: $('.js-friend', friend).html()};
-  //   }
-  // }
-
-  // // update the UI to show which friends added
-  // function updateAdded() {
-  //   var FriendBlockTemplate = Handlebars.compile($('#friend_block_template').html());
-  //   // Populate friends list
-  //   $('.friend_group_row .friend_block').remove();
-  //   console.log(friendsToAdd);
-  //   $('.friend_group_row').prepend(FriendBlockTemplate({ friends: friendsToAdd }));
-  // }
 
 });
